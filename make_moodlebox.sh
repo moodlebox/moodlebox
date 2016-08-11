6cc1a34169eccdad6fa8d085deeeb70a5b8f9424
Nicolas Martignoni <nicolas@martignoni.net>
2016-08-11 14:42:25 +0200

    Fixed wrong directory name

--- make_moodlebox.sh
+++ make_moodlebox.sh
@@ -356,7 +356,7 @@ EOF
     ## Install MoodleBox Admin Moodle plugin
     echo -e "\e[93mMoodleBox plugin installation (via CLI)...\e[97m"
     cd /var/www/html/admin/tool/
-    git clone https://github.com/martignoni/moodlebox-plugin.git
+    git clone https://github.com/martignoni/moodlebox-plugin.git moodlebox
     cd /var/www/html/admin/tool/moodlebox
     touch .reboot-server; touch .shutdown-server; touch .set-server-datetime
     chown -R www-data:www-data /var/www/html/admin/tool/moodlebox
