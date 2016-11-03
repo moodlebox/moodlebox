3b5a575ffa078ef382b8bcc87254aba1e01ef682
Nicolas Martignoni <nicolas@martignoni.net>
2016-11-03 20:28:04 +0100

    Optimize cloning of Moodle stable branch, using a shallow clone

--- make_moodlebox.sh
+++ make_moodlebox.sh
@@ -322,10 +322,7 @@ STOP
     ## Download Moodle via git and create all needed directories, with adequate permissions
     echo -e "\e[93mDownloading Moodle 3.1.x via Git and directories configuration...\e[97m"
     cd /var/www/
-    git clone git://git.moodle.org/moodle.git
-    cd moodle
-    git checkout MOODLE_31_STABLE
-    cd ..
+    git clone --depth=1 -b MOODLE_31_STABLE git://git.moodle.org/moodle.git
     rm -r html
     mv moodle html
     chown -R www-data:www-data /var/www/html
