5dd68f5141deb69566f933e3ff006c5e6c61976f
Nicolas Martignoni <nicolas@martignoni.net>
2016-11-05 14:23:12 +0100

    Removing non pertinent file

--- util/cpu_temp.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-/bin/date +%T | awk '{print "time="$1}'
-awk '{print "gov="$1}' /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-awk '{print "freq="$1/1000"Mhz"}' /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq
-awk '{print "temp="$1/1000"°C"}' /sys/class/thermal/thermal_zone0/temp
-df | awk '$NF == "/" { print "sdcard avail="sprintf("%.2f", $4/(1024*1024))"GB ("sprintf("%.2f", 100*$4/$2)"%)" }'
-#/opt/vc/bin/vcgencmd measure_clock arm
-#/opt/vc/bin/vcgencmd measure_temp
-/opt/vc/bin/vcgencmd measure_volts core
