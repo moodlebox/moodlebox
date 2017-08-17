d40080e577498cdfffbec8c06696bc88665abe7a
Nicolas Martignoni <nicolas@martignoni.net>
2017-08-18 00:33:33 +0200

    File content /etc/network/interfaces updated

--- make_moodlebox.sh
+++ make_moodlebox.sh
@@ -290,13 +290,18 @@ EOF
     cat << "EOF" > /etc/network/interfaces
 # interfaces(5) file used by ifup(8) and ifdown(8)
 
+# Please note that this file is written to be used with dhcpcd
+# For static IP, consult /etc/dhcpcd.conf and 'man dhcpcd.conf'
+
 # Include files from /etc/network/interfaces.d:
 source-directory /etc/network/interfaces.d
 
 auto lo
 iface lo inet loopback
 
-iface eth0 inet manual
+auto eth0
+iface eth0 inet dhcp
+iface eth0 inet6 auto
 
 allow-hotplug wlan0
 iface wlan0 inet static
@@ -304,7 +309,6 @@ iface wlan0 inet static
     netmask 255.255.255.0
     network 10.0.0.0
     broadcast 10.0.0.255
-#    wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
 
 allow-hotplug wlan1
 iface wlan1 inet manual
